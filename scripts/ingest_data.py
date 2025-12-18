5d19c410cd97bc0af72c8aa178688ba93bb2956e
rashidnm <arshialvi@hotmail.co.uk>
2025-12-18 22:37:17 +0000

    feat: Update ingest_data.py to add JSON key
    feat: Update ingest_data.py to add JSON key

--- scripts/ingest_data.py
+++ scripts/ingest_data.py
@@ -2,13 +2,22 @@ import yfinance as yf
 from google.cloud import bigquery
 import pandas as pd
 
+import os
+from google.cloud import bigquery
+
+# Tell Python where your bot's "key" is
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "config/my-data-portfolio-481622-400a944d1804.json"
+
+client = bigquery.Client()
+# Now 'client' acts as your Service Account bot!
+
 # 1. Fetch Data
 ticker = "TSLA"
 data = yf.download(ticker, period="1mo", interval="1d")
 data.reset_index(inplace=True)
 
 # 2. BigQuery Setup
-client = bigquery.Client()
+# client = bigquery.Client()
 table_id = "your-project.your_dataset.raw_stock_data"
 
 # 3. Load to BigQuery
